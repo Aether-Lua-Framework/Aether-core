5c863763ddba3ea105bc9f4e521e9eecf5a5f0c7
junhyeok <junhyeok@junhyeokui-MacBookPro.local>
2026-08-25 15:25:38 +0900

    feat: needsLibrary meta_data added

--- src/aether/dlc/crypto/init.lua
+++ src/aether/dlc/crypto/init.lua
@@ -3,11 +3,11 @@ local hmac = require("openssl.hmac")
 return {
     name = "crypto",
     requires = "",
+    needsLibrary = { "openssl.hmac" },
     provides = { "crypto" },
-
     setup = function(app)
         app.crypto = {
-            -- sign with HAMC SHA256
+            -- sign with HMAC SHA256
             hmacSha256 = function(k, msg)
                 local h = hmac.new(k, "sha256")
                 h:update(msg)
