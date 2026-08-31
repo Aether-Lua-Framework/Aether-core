7c6c910ec84b69e5fa7104f232c4e8685fc8be0b
JJH090501 <cruisesa542@gmail.com>
2026-08-31 17:05:06 +0900

    feat: Added serve()

--- src/aether/dlc/tcp/init.lua
+++ src/aether/dlc/tcp/init.lua
@@ -15,5 +15,31 @@ return {
         app.listen = function(_, host, port)
             return socket.listen(host, port)
         end
-    end
+
+        app.serve = function(_, host, port, handler)
+            app._loop:spawn(function()
+                local server, err = socket.listen(host, port)
+                if not server then
+                    print("listen error: ", tostring(err))
+                    return
+                end
+                
+                print("Aether listening on " .. host .. ":" .. port)
+
+
+                while true do
+                    local conn, aerr = server:accept()
+                    if conn then
+                        -- new task spawn
+                        app._loop:spawn(function()
+                            handler(conn)
+                        end)
+                    else
+                        print("accept error : " .. aerr)
+                    end
+                end
+            end)
+        end
+        
+    end,
 }
