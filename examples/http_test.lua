12ca683309fef8b0c8dee81ab7b7f4ff466f39ec
JJH090501 <cruisesa542@gmail.com>
2026-09-08 16:54:03 +0900

    test: logger loading added

--- examples/http_test.lua
+++ examples/http_test.lua
@@ -1,9 +1,10 @@
 local kernel = require("aether.kernel")
 local tcp = require("aether.dlc.tcp")
 local http = require("aether.dlc.http")
+local logger = require("aether.dlc.logger")
 
 -- 순서 상관없음 (위상 정렬). http를 먼저 넣어봐도 됨.
-local app = kernel.build({ http, tcp })
+local app = kernel.build({ http, tcp, logger })
 
 app:get("/", function()
     return "Hello from Aether HTTP!"
