8ba8b043cd23b7742da5d3192668fcbd08681572
JJH090501 <cruisesa542@gmail.com>
2026-09-08 18:02:32 +0900

    test: test case comment deleted

--- examples/crud_test.lua
+++ examples/crud_test.lua
@@ -9,13 +9,13 @@ app.store:create("users")
 app.store:create("posts")
 
 app:post("/users", function(req)
-    local users = app.store:table("users")     -- 고침
+    local users = app.store:table("users")
     local id = users:insert(req.body)
     return "created user: " .. id
 end)
 
 app:get("/users", function(req)
-    local users = app.store:table("users")     -- 고침
+    local users = app.store:table("users")
     local lines = {}
     for k, v in pairs(users:all()) do
         lines[#lines+1] = k .. " = " .. v
@@ -24,13 +24,13 @@ app:get("/users", function(req)
 end)
 
 app:post("/posts", function(req)
-    local posts = app.store:table("posts")     -- 고침
+    local posts = app.store:table("posts")
     local id = posts:insert(req.body)
     return "created post" .. id
 end)
 
 app:get("/posts", function(req)
-    local posts = app.store:table("posts")     -- 고침
+    local posts = app.store:table("posts")
     local lines = {}
     for k, v in pairs(posts:all()) do
         lines[#lines+1] = k .. " = " .. v
@@ -39,7 +39,7 @@ app:get("/posts", function(req)
 end)
 
 app:get("/broken", function(req)
-    local x = app.store:table("userss")        -- 고침
+    local x = app.store:table("userss")
     return "should not reach"
 end)
 
